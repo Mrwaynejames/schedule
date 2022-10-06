780aa1d13c58a7b1eedfac2caf627efc1a90ecb7
wayne james <jackmcwilliams@Jacks-Air.lan>
2022-10-06 00:22:04 -0400

    made class name changes

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 var today = moment();
 $("#currentDay").text(today.format("ddd MMM/Do/YYYY"));
+
+//var saveButton = document.getElementsByClassName("save");
+//saveButton.setAttribute("style", "backgroundColor:blue");
+
+var btnEl = document.querySelectorAll("button");
+
+for (var i = 0; i < btnEl.length; i++) {
+btnEl[i].setAttribute("style", "color:blue");
+}
